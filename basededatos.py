94afefcad4cb78d5bcaf985a42b9c06ef0a71e78
Alejandro <alejandrocoxaj94@gmail.com>
2026-05-12 11:06:56 -0600

    Alejandro corrigió un dato en la base de datos.

--- basededatos.py
+++ basededatos.py
@@ -26,7 +26,6 @@ def preparar_sistema_completo():
         cursor = conexion.cursor()
         cursor.execute("PRAGMA foreign_keys = ON")
 
-        # TABLA USUARIOS
         cursor.execute('''CREATE TABLE usuarios (
             id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre_completo TEXT NOT NULL,
@@ -34,7 +33,6 @@ def preparar_sistema_completo():
             password TEXT NOT NULL,
             rol TEXT NOT NULL)''')
 
-        # TABLA CLIENTES
         cursor.execute('''CREATE TABLE clientes (
             id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre_empresa TEXT UNIQUE NOT NULL,
@@ -43,7 +41,6 @@ def preparar_sistema_completo():
             direccion_empresa TEXT,
             uuid_empresa TEXT UNIQUE NOT NULL)''')
 
-        # TABLA PUBLICIDAD (Ajustada para guardar rutas largas)
         cursor.execute('''CREATE TABLE publicidad (
             id_pub INTEGER PRIMARY KEY AUTOINCREMENT, 
             uuid_empresa TEXT, 
@@ -52,7 +49,6 @@ def preparar_sistema_completo():
             fecha TEXT,
             FOREIGN KEY (uuid_empresa) REFERENCES clientes (uuid_empresa) ON DELETE CASCADE)''')
 
-        # TABLA PROYECTOS
         cursor.execute('''CREATE TABLE proyectos (
             id_proyecto INTEGER PRIMARY KEY AUTOINCREMENT,
             id_cliente INTEGER,
@@ -62,14 +58,12 @@ def preparar_sistema_completo():
             estado TEXT,
             FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente))''')
 
-        # TABLA MENSAJES
         cursor.execute('''CREATE TABLE mensajes (
             id_mensaje INTEGER PRIMARY KEY AUTOINCREMENT,
             remitente TEXT NOT NULL,
             contenido TEXT NOT NULL,
             fecha_hora TEXT NOT NULL)''')
 
-        # --- DATOS INICIALES ---
         usuarios = [
             ('Alejandro Coxaj', 'alejandro', 'prog123', 'Programador'),
             ('Francisco Contreras', 'francisco', 'foto345', 'Fotógrafo')
@@ -78,7 +72,6 @@ def preparar_sistema_completo():
             'INSERT INTO usuarios (nombre_completo, usuario_login, password, rol) VALUES (?, ?, ?, ?)',
             usuarios)
 
-        # Cliente de prueba
         id_prueba = generar_uuid()
         cursor.execute('''INSERT INTO clientes 
             (nombre_empresa, telefono_referido, correo, direccion_empresa, uuid_empresa) 
